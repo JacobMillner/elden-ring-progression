173fdea0e0392cd0a9baaddd24f79fd88f224a14
Jacob Millner <jacob.millner@outlook.com>
2022-03-12 15:54:12 -0500

    a bit of tailwind, good enough 🤷

--- src/components/ProgressRow/index.tsx
+++ src/components/ProgressRow/index.tsx
@@ -6,9 +6,32 @@ export interface ProgressRowProps {
   link?: string;
 }
 
-const ProgressRow = (props: ProgressRow) => {
+const ProgressRow = (props: ProgressRowProps) => {
   const { id, title, checked, image, link } = props;
-  return <div>foo</div>;
+  return (
+    <tr
+      id={id}
+      className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+    >
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        <input
+          className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+          type="checkbox"
+          checked={checked}
+          id="flexCheckDefault"
+        />
+      </td>
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {title}
+      </td>
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {link}
+      </td>
+      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {image}
+      </td>
+    </tr>
+  );
 };
 
 export default ProgressRow;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,92 @@ import ProgressRow from 'components/ProgressRow';
 export default function Home() {
   return (
     <>
-      <div className="p-6 shadow-lg rounded-lg bg-gray-100 text-gray-700">
+      <div className="mb-10 p-6 shadow-lg rounded-lg bg-gray-100 text-gray-700">
         <main>
-          <h2 class="font-semibold text-3xl mb-5">
+          <h2 className="font-semibold text-3xl mb-5">
             Elden Ring Progression Guide and Tracker
           </h2>
         </main>
       </div>
-      <h1>Test Table</h1>
-      <table class="table-auto">
-        <thead>
-          <tr>
-            <th>Complete</th>
-            <th>Name</th>
-            <th>Link</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          <ProgressRow />
-        </tbody>
-      </table>
+
+      <div className="accordion" id="accordionExample5">
+        <div className="accordion-item bg-white border border-gray-200">
+          <h2 className="accordion-header mb-0" id="headingOne5">
+            <button
+              className="
+        accordion-button
+        relative
+        flex
+        items-center
+        w-full
+        py-4
+        px-5
+        text-base text-gray-800 text-left
+        bg-white
+        border-0
+        rounded-none
+        transition
+        focus:outline-none
+      "
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne5"
+              aria-expanded="true"
+              aria-controls="collapseOne5"
+            >
+              West Limegrave  
+            </button>
+          </h2>
+          <div
+            id="collapseOne5"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne5"
+          >
+            <div className="accordion-body py-4 px-5">
+              <table className="table-auto">
+                <thead>
+                  <tr>
+                    <th>Done</th>
+                    <th>Task</th>
+                    <th>Link</th>
+                    <th>Image</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <ProgressRow
+                    checked={false}
+                    id="1"
+                    title="lets do the thing"
+                    image="image/src.jpg"
+                    link="google.com"
+                  />
+                  <ProgressRow
+                    checked={false}
+                    id="1"
+                    title="lets do the thing"
+                    image="image/src.jpg"
+                    link="google.com"
+                  />
+                  <ProgressRow
+                    checked={false}
+                    id="1"
+                    title="lets do the thing"
+                    image="image/src.jpg"
+                    link="google.com"
+                  />
+                  <ProgressRow
+                    checked={false}
+                    id="1"
+                    title="lets do the thing"
+                    image="image/src.jpg"
+                    link="google.com"
+                  />
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
