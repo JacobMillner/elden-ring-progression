2fda6ae5e3005db16c7a729864a838f558104603
Jacob Millner <jacob.millner@outlook.com>
2022-03-12 20:44:04 -0500

    github pages action

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>NextJS TailwindCSS TypeScript Starter</title>
+        <title>Elden Ring Progress</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
