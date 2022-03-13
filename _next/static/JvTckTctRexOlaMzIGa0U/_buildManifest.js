9b2ec3d59f2a48826cccf97e53251c24f52d2d2e
JacobMillner <jacob.millner@outlook.com>
2022-03-13 05:48:51 +0000

    Deploying to gh-pages from @ JacobMillner/elden-ring-progression@071e7600c6d9c4035c92017278c8099bb4686691 🚀

---
+++ _next/static/JvTckTctRexOlaMzIGa0U/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/485-45b2246573badcb9.js","static/chunks/pages/index-2652aeed56576c76.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
