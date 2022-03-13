9cb36851defc3e1289f4092511ddb5214755d41c
Jacob Millner <jacob.millner@outlook.com>
2022-03-13 02:11:51 -0400

    oops remove console logs

--- src/hooks/useLocalstorage/index.ts
+++ src/hooks/useLocalstorage/index.ts
@@ -6,26 +6,21 @@ const useLocalStorage = (keyName: string, defaultValue: string) => {
       const value = window.localStorage.getItem(keyName);
 
       if (value) {
-        console.log('value found!');
         return JSON.parse(value);
       } else {
-        console.log('setting localStorage');
         window.localStorage.setItem(keyName, JSON.stringify(defaultValue));
         return defaultValue;
       }
     } catch (err) {
-      console.log('error', err);
       return defaultValue;
     }
   });
 
   const setValue = (newValue: string) => {
     try {
-      console.log('setting', JSON.stringify(newValue));
       window.localStorage.setItem(keyName, JSON.stringify(newValue));
     } catch (err) {
       // TODO: do something with this error
-      console.log('seerrtting', err);
     }
     setStoredValue(newValue);
   };
