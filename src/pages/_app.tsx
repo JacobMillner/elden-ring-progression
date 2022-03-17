a184fbb8ea32a560197305b45f5fc3a8a9fa580f
Jacob Millner <jacob.millner@outlook.com>
2022-03-16 20:49:04 -0400

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,14 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Elden Ring Progress</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E626VPKZML"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-E626VPKZML');
+        </script>
       </Head>
       <Component {...pageProps} />
     </>
